c9a2bb4c09453c6a9a2cd77c85b247951ef21a11
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-02-05 16:20:25 -0300

    a

---
+++ app/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import {StyleSheet, Text, View, Button} from "react-native";
+
+export default function App(){
+    const[count, setCount] = useState(0);
+
+
+ return(
+    <View style={styles.container}>
+        <Text style={styles.text}>Clique para contar: {count}</Text>
+        <Button
+        title="Clique"
+        onPress={()=> setCount(count+1)}
+        />
+    </View>
+)
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "#fff",
+        alignItems:"center",
+        justifyContent:"center",
+        text:{
+            fontSize:"2px"
+        }
+    }
+
+    
+})
